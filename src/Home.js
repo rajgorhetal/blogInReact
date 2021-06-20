61d525fd1c7e002e36e73511780fdf0174f86b86
Hetal <hetalrajgorb@gmail.com>
2021-06-21 01:22:04 +0530

    useEffect Hooks Example with dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,30 @@
-import { useState } from "react";
-
-
-
-
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
-    const [name,setName] = useState('mario');
-    const [age, setAge] = useState(25);
+    //const [name, setName] = useState('mario');
 
-    const handleClick = () =>{
-        setName('luigi');
-        setAge(30);
-     }
-    // const handleClickAgain = (name,e) => {
-    //     console.log('Hello '+ name, e.target);
-    // }
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    };
+    // useEffect(
+    //     () => {
+    //         console.log('use Effect Ran');
+    //         console.log(name);
+    //     },[name]);
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Me</button>
-            {/* <button onClick={(e) => handleClickAgain('mario',e)}>click me again</button> */}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/* <button onClick = {() => setName('Luigi')}>Change Name</button> */}
+            {/* <p>{name}</p> */}
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs!"/> */}
         </div>
      );
 }
